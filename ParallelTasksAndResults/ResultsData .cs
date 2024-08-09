39140191f9b2fa0d00a9b20c6a9457c21759600d
emaye <edremoda2001@yahoo.com>
2024-08-09 12:54:56 +0100

    Create ResultsData .cs

---
+++ ParallelTasksAndResults/ResultsData .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTasksAndResults
+{
+    internal class ResultsData
+    {
+        public string? SearchType
+        {
+            get;
+            set;
+        }
+        public int NumberOfResults
+        {
+            get;
+            set;
+        }
+    }
+}
